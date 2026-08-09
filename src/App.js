bd7190c3483005052b152407905001f7933c66aa
Erin Wilson <ewilson001500@gmail.com>
2026-08-09 14:53:33 -0500

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,26 +3,28 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <h1>Weather App</h1>
+      <div className="container">
+        <h1>Weather App</h1>
 
-      <footer>
-        Coded by{" "}
-        <a
-          href="https://github.com/ewilson001500-tech"
-          target="_blank"
-          rel="noreferrer"
-        >
-          Erin Wilson
-        </a>{" "}
-        and is open-sourced on{" "}
-        <a
-          href="https://github.com/ewilson001500-tech/weather-react-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          GitHub
-        </a>
-      </footer>
+        <footer>
+          Coded by{" "}
+          <a
+            href="https://github.com/ewilson001500-tech"
+            target="_blank"
+            rel="noreferrer"
+          >
+            Erin Wilson
+          </a>{" "}
+          and is open-sourced on{" "}
+          <a
+            href="https://github.com/ewilson001500-tech/weather-react-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            GitHub
+          </a>
+        </footer>
+      </div>
     </div>
   );
 }
