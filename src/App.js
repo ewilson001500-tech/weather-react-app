0d51f7b66d157429c4b20d2d98f06eb4c7920310
Erin Wilson <ewilson001500@gmail.com>
2026-08-09 14:30:53 -0500

    Added the GitHub repo URL

--- src/App.js
+++ src/App.js
@@ -1,5 +1,23 @@
 import "./App.css";
 
 export default function App() {
-  return <div className="App">Weather App</div>;
+  return (
+    <div className="App">
+      <h1>Weather App</h1>
+
+      <footer>
+        Coded by{" "}
+        <a href="https://github.com/ewilson001500-tech" target="_blank">
+          Erin Wilson
+        </a>{" "}
+        and is open-sourced on{" "}
+        <a
+          href="https://github.com/ewilson001500-tech/weather-react-app"
+          target="_blank"
+        >
+          GitHub
+        </a>
+      </footer>
+    </div>
+  );
 }
