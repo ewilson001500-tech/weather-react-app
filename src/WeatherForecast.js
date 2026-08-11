1226bd522b75c4c00da3cb9489b974f3010b02b6
Erin Wilson <ewilson001500@gmail.com>
2026-08-11 14:40:11 -0500

    Added the layout for the forecast

---
+++ src/WeatherForecast.js
@@ -0,0 +1,14 @@
+import React from "react";
+import "./WeatherForecast.css";
+
+export default function WeatherForecast() {
+  return (
+    <div className="WeatherForecast">
+      <h5>Tue</h5>
+      icon
+      <p>
+        <strong>93°</strong> 79°
+      </p>
+    </div>
+  );
+}
